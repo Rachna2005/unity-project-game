84cf2286334f1c979146303abfce628efe39db3d
sutheavy22 <sutheavy22@gmail.com>
2025-12-20 14:26:42 +0700

    Car scene

---
+++ Assets/scripts/CarSelection.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CarSelection : MonoBehaviour
+{
+    public GameObject[] cars;   
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        ShowCar(currentIndex);
+    }
+
+    void ShowCar(int index)
+    {
+        for (int i = 0; i < cars.Length; i++)
+        {
+            cars[i].SetActive(i == index);
+        }
+    }
+
+     public void RightArrow()
+    {
+        currentIndex++;
+
+        if (currentIndex >= cars.Length)
+            currentIndex = 0;
+
+        ShowCar();
+    }
+
+    public void LeftArrow()
+    {
+        currentIndex--;
+
+        if (currentIndex < 0)
+            currentIndex = cars.Length - 1;
+
+        ShowCar();
+    }
+}
